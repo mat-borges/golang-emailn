ee40d476d1f3253538f95a853f25a9cfa8b7b0ad
Mateus Borges <borgesm.work@gmail.com>
2025-10-06 20:05:19 -0300

    feat: Add main application entry point with campaign endpoint setup

---
+++ cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"emailn/internal/domain/campaign"
+	"emailn/internal/endpoints"
+	"emailn/internal/infrasctructure/database"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	campaignService := campaign.Service{
+		Repo: &database.CampaignRepository{},
+	}
+	handler := endpoints.Handler{CampaignService: campaignService}
+
+	r.Post("/campaigns", handler.CampaignPost)
+
+	http.ListenAndServe(":3000", r)
+}
