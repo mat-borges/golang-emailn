50cd28dc4432b4d753ff4d8786255dc57107fc89
Mateus Borges <borgesm.work@gmail.com>
2025-10-06 20:24:19 -0300

    feat: Refactor campaign endpoints to use error handling middleware

--- internal/endpoints/campaigns_get.go
+++ internal/endpoints/campaigns_get.go
@@ -1,24 +1,11 @@
 package endpoints
 
 import (
-	internalerrors "emailn/internal/internal-errors"
-	"errors"
 	"net/http"
-
-	"github.com/go-chi/render"
 )
 
-func (h Handler) CampaignGet(w http.ResponseWriter, r *http.Request) {
+func (h Handler) CampaignGet(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
 	campaigns, err := h.CampaignService.Repo.GetAll()
-	if err != nil {
-		if errors.Is(err, internalerrors.ErrInternal) {
-			render.Status(r, http.StatusInternalServerError)
-		} else {
-			render.Status(r, http.StatusBadRequest)
-		}
-		render.JSON(w, r, map[string]string{"error": err.Error()})
-		return
-	}
-	render.Status(r, http.StatusOK)
-	render.JSON(w, r, campaigns)
+
+	return campaigns, http.StatusOK, err
 }
--- internal/endpoints/campaigns_post.go
+++ internal/endpoints/campaigns_post.go
@@ -2,28 +2,15 @@ package endpoints
 
 import (
 	"emailn/internal/contract"
-	internalerrors "emailn/internal/internal-errors"
-	"errors"
 	"net/http"
 
 	"github.com/go-chi/render"
 )
 
-func (h Handler) CampaignPost(w http.ResponseWriter, r *http.Request) {
+func (h Handler) CampaignPost(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
 	var request contract.NewCampaignDto
 	render.DecodeJSON(r.Body, &request)
 	id, err := h.CampaignService.Create(request)
 
-	if err != nil {
-		if errors.Is(err, internalerrors.ErrInternal) {
-			render.Status(r, http.StatusInternalServerError)
-		} else {
-			render.Status(r, http.StatusBadRequest)
-		}
-		render.JSON(w, r, map[string]string{"error": err.Error()})
-		return
-	}
-
-	render.Status(r, http.StatusCreated)
-	render.JSON(w, r, map[string]string{"id": id})
+	return map[string]string{"id": id}, http.StatusCreated, err
 }
